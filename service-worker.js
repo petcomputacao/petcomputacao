21f8140534193e157bd56206fd3aae18a4bbc6ee
dayvidds <dayvid.silva@ccc.ufcg.edu.br>
2021-10-27 18:58:52 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/segpet-frontend/precache-manifest.0744018a0b4c0272a4e3f2c30f1c65e8.js"
+  "/petcomputacao/precache-manifest.7a91eb5f5ed14e2810991488cbf1234f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/segpet-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/petcomputacao/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
